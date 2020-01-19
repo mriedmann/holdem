0c2261a8a7b49281a44a89f40cd6b72e7b62cdbb
Michael Riedmann <michael_riedmann@live.com>
2020-01-19 19:23:34 +0100

    basic test refactoring and FsUnit integration

--- src/FsFhtw/Helpers.fs
+++ src/FsFhtw/Helpers.fs
@@ -2,19 +2,19 @@ module Helpers
 
 open System
 
-let mutable rnd = Random()
-
-let SeedRandom (seed:int) =
-    rnd <- Random(seed)
-
-let KnuthShuffle (lst : array<'a>) =                   // '
-    let Swap i j =                                                  // Standard swap
+let KnuthShuffle (seed:int option) (lst : array<'a>) =   
+    let rnd = 
+        match seed with
+        | Some s -> Random(s)
+        | None -> Random()
+                    // '
+    let swap i j =                                                  // Standard swap
         let item = lst.[i]
         lst.[i] <- lst.[j]
         lst.[j] <- item
     let ln = lst.Length
     [0..(ln - 2)]                                                   // For all indices except the last
-    |> Seq.iter (fun i -> Swap i (rnd.Next(i, ln)))                 // swap th item at the index with a random one following it (or itself)
+    |> Seq.iter (fun i -> swap i (rnd.Next(i, ln)))                 // swap th item at the index with a random one following it (or itself)
     lst   
 
 let notImplemented () =
